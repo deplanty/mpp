074463997162b897490dc63c67c36b44a3615f6f
deplanty <yvon.deplante@gmail.com>
2020-04-09 16:05:51 +0200

    Ask to install PyInstaller for the user

--- src/commands/freeze.py
+++ src/commands/freeze.py
@@ -3,11 +3,24 @@ import os
 import shutil
 import sys
 
+from src.utils import ask
 
 def freeze(args):
     # PyInstaller exists
     if not shutil.which("pyinstaller"):
-        sys.exit("It seems that PyInstaller is not installed.\nPlease, consider using `pip install PyInstaller`.")
+        print("It seems that PyInstaller is not installed.")
+        print("Please, consider using `pip install PyInstaller`.")
+        print(f"Current pip is {shutil.which('pip')}.")
+        print("")
+        answer = ask.question("Do you want to install it on the go (y/n)?", "n")
+        if answer == "y":
+            print("")
+            print("~$ pip install PyInstaller")
+            os.system("pip install PyInstaller")
+        else:
+            sys.exit()
 
     os.chdir("installer")
+    print("")
+    print("~$ pyinstaller installer.spec")
     os.system("pyinstaller installer.spec")
